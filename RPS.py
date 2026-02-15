5509255551a6f3751ebdad653b905b4101fe3aee
Gustavo-Vargas06 <gustavovargas@unomaha.edu>
2026-02-15 19:48:11 +0000

    RPS Fin

--- RPS.py
+++ RPS.py
@@ -1,7 +1,7 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Gustavo Vargas
+#Date: 02/15/2026
+#Assignment: RPS
 import random
 
 def main():
@@ -10,24 +10,45 @@ def main():
   losses = 0
   #Create a loop that continues as long as the user wants to play.
   again = "Y"
-  while again == "Y" or "y":
-    game = input("Want to play a game? [Y/N] :")
-    if game == "Y" or game == "y":
-      RPS = input("Ok! Rock, Paper, or Scissors? [R/P/S]")
-      answers = random.choice(["Rock", "Paper", "Scissors"])
-      print("I chose", answers)
-      if answers == "Rock" and RPS == "S":
-        print("Oh no! Better luck next time!")
-        losses = losses + 1
-      if answers == "Rock" and RPS == "P":
-        print("Holy smokes you got me. Good Job!")
-        wins = wins + 1
-      if answers == "Rock" and RPS == "R":
-        print("WOW, we managed to pick the same thing...")
-        ties = ties + 1
-  
+  Game = input("Want to play a silly game? [Y/N]: ")
+  if Game == "Y" or Game == "y":
+      while again == "Y" or again == "y":
+        RPS = input("Rock, Paper, or Scissors? [R/P/S]")
+        answers = random.choice(["Rock", "Paper", "Scissors"])
+        print("I chose", answers)
+        if answers == "Rock" and RPS == "S":
+          print("Oh no! Better luck next time!")
+          losses = losses + 1
+        if answers == "Rock" and RPS == "P":
+          print("Holy smokes you got me. Good Job!")
+          wins = wins + 1
+        if answers == "Rock" and RPS == "R":
+          print("WOW, we managed to pick the same thing...")
+          ties = ties + 1
+        if answers == "Paper" and RPS == "R":
+          print("Oh no! Better luck next time!")
+          losses = losses + 1
+        if answers == "Paper" and RPS == "S":
+          print("Holy smokes you got me. Good Job!")
+          wins = wins + 1
+        if answers == "Paper" and RPS == "P":
+          print("WOW, we managed to pick the same thing...")
+          ties = ties + 1
+        if answers == "Scissors" and RPS == "P":
+          print("Oh no! Better luck next time!")
+          losses = losses + 1
+        if answers == "Scissors" and RPS == "R":
+          print("Holy smokes you got me. Good Job!")
+          wins = wins + 1
+        if answers == "Scissors" and RPS == "S":
+          print("WOW, we managed to pick the same thing...")
+          ties = ties + 1
+        again = input("Want to play again? [Y/N] :")
+      else:
+        print("Bye, Have fun somewhere else!")
+        
 
- 
+  else:
     print("Bye, Have fun somewhere else!")
 
 
